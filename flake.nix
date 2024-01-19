7fe37fc6455e2a37f4a4f14faecae021078fd397
Millian Poquet <millian.poquet@irit.fr>
2024-01-19 17:15:01 +0100

    nix: initial flake (just a single shell)

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in rec {
+        devShells = {
+          dev = pkgs.mkShell {
+            buildInputs = [
+              pkgs.typst
+              pkgs.python3
+              pkgs.python3Packages.opencv4
+              pkgs.python3Packages.pytest
+            ];
+          };
+        };
+      }
+    );
+}
