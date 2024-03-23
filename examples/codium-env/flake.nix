d595b886f5389cd710d62d45e475d3e10fa24e30
Millian Poquet <millian.poquet@irit.fr>
2024-03-23 07:22:58 +0100

    wip: codium nix

---
+++ examples/codium-env/flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    typst = {
+      url = "github:typst/typst";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.systems.follows = "flake-utils/systems";
+    };
+    nix-vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+          extensions = inputs.nix-vscode-extensions.extensions.${system};
+          typstPkgs = inputs.typst.packages.${system};
+          inherit (pkgs) vscode-with-extensions vscodium;
+
+          packages.default =
+            vscode-with-extensions.override {
+              vscode = vscodium;
+              vscodeExtensions = [
+                extensions.open-vsx.nvarner.typst-lsp
+              ];
+            };
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = [ packages.default typstPkgs.default pkgs.typst-lsp ];
+            shellHook = ''
+              printf "VSCodium with extensions:\n"
+              codium --list-extensions
+            '';
+          };
+        in
+        {
+          inherit packages devShells;
+        });
+}
